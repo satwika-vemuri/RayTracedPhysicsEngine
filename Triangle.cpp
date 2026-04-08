ddde6bb19c7246c0ecacae93d552a3c50e9f7eba
Satwika Vemuri <satwika.vemuri@gmail.com>
2026-04-07 21:01:36 -0700

    first

---
+++ Triangle.cpp
@@ -0,0 +1,16 @@
+class Point{
+    double x;
+    double y;
+};
+class Vector{
+    double x;
+    double y; 
+    double z;
+};
+class Triangle{
+    Point v1;
+    Point v2;
+    Point v3;
+
+    Vector normal;
+};

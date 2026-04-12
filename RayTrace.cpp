1a420adb41058b899aeb42bc11235bacf2fa4f11
Satwika Vemuri <satwika.vemuri@gmail.com>
2026-04-12 09:47:02 -0700

    swapped y and z axis appropriately

--- RayTrace.cpp
+++ RayTrace.cpp
@@ -10,52 +10,52 @@ const int IMAGE_WIDTH = 600;
 const int IMAGE_HEIGHT = 800;
 const double PI = 2 * acos(0.0);
 
-class Point {
+class Point3 {
 public:
     double x;
     double y;
     double z;
-    Point(double a, double b, double c) : x(a), y(b), z(c) {}
+    Point3(double a, double b, double c) : x(a), y(b), z(c) {}
 };
 
-class Vector { // defined by a start point and direction
+class Vector3 { // defined by a start point and direction
 public:
-    Point p;
+    Point3 p;
     double dx;
     double dy; 
     double dz;
 
-    Vector(): p(Point(0, 0, 0)), dx(0), dy(0), dz(0){}
-    Vector(double x, double y, double z): p(Point(0, 0, 0)), dx(x), dy(y), dz(z){}
-    Vector(Point a, double x, double y, double z) : p(a), dx(x), dy(y), dz(z) {}
+    Vector3(): p(Point3(0, 0, 0)), dx(0), dy(0), dz(0){}
+    Vector3(double x, double y, double z): p(Point3(0, 0, 0)), dx(x), dy(y), dz(z){}
+    Vector3(Point3 a, double x, double y, double z) : p(a), dx(x), dy(y), dz(z) {}
 };
 
 class Triangle{
 public:
-    Point v1;
-    Point v2;
-    Point v3;
+    Point3 v1;
+    Point3 v2;
+    Point3 v3;
 
-    Vector n1; // normal at point v1
-    Vector n2; // normal at point v2
-    Vector n3; // normal at point v3
-    Vector triangleNormal;
-    Triangle(Point a, Point b, Point c): v1(a), v2(b), v3(c) {}
+    Vector3 n1; // normal at point v1
+    Vector3 n2; // normal at point v2
+    Vector3 n3; // normal at point v3
+    Vector3 triangleNormal;
+    Triangle(Point3 a, Point3 b, Point3 c): v1(a), v2(b), v3(c) {}
 };
 
 struct HitInfo {
     bool hit;
-    Point point;
+    Point3 point;
     double distance;
 
-    HitInfo(bool h, Point p, double d) : hit(h), point(p), distance(d) {}
+    HitInfo(bool h, Point3 p, double d) : hit(h), point(p), distance(d) {}
 };
 
 struct Intersection{
     Triangle* tri;
-    Point intersection;
+    Point3 intersection;
 
-    Intersection(Triangle* tri, Point p);
+    Intersection(Triangle* tri, Point3 p);
 };
 
 /**
@@ -70,14 +70,14 @@ vector<Triangle*> constructSceneTriangles(vector<vector<int>> vertexBuffer, vect
     vector<Triangle*> sceneTriangles;
 
     for(int i = 0; i < indexBuffer.size()-2; i++){
-        Point v1(vertexBuffer[i][0], vertexBuffer[i][1], vertexBuffer[i][2]);
-        Point v2(vertexBuffer[i+1][0], vertexBuffer[i+1][1], vertexBuffer[i+1][2]);
-        Point v3(vertexBuffer[i+2][0], vertexBuffer[i+2][1], vertexBuffer[i+2][2]);
+        Point3 v1(vertexBuffer[i][0], vertexBuffer[i][1], vertexBuffer[i][2]);
+        Point3 v2(vertexBuffer[i+1][0], vertexBuffer[i+1][1], vertexBuffer[i+1][2]);
+        Point3 v3(vertexBuffer[i+2][0], vertexBuffer[i+2][1], vertexBuffer[i+2][2]);
 
-        Vector n1(normalBuffer[i][0], normalBuffer[i][1], normalBuffer[i][2]);
-        Vector n2(normalBuffer[i+1][0], normalBuffer[i+1][1], normalBuffer[i+1][2]);
-        Vector n3(normalBuffer[i+2][0], normalBuffer[i+2][1], normalBuffer[i+2][2]);
-        Vector tn((n1.dx+n2.dx+n3.dx)/3, (n1.dy+n2.dy+n3.dy)/3, (n1.dz+n2.dz+n3.dz)/3);
+        Vector3 n1(normalBuffer[i][0], normalBuffer[i][1], normalBuffer[i][2]);
+        Vector3 n2(normalBuffer[i+1][0], normalBuffer[i+1][1], normalBuffer[i+1][2]);
+        Vector3 n3(normalBuffer[i+2][0], normalBuffer[i+2][1], normalBuffer[i+2][2]);
+        Vector3 tn((n1.dx+n2.dx+n3.dx)/3, (n1.dy+n2.dy+n3.dy)/3, (n1.dz+n2.dz+n3.dz)/3);
 
         Triangle* t = new Triangle(v1, v2, v3);
         t->n1 = n1;
@@ -92,19 +92,20 @@ vector<Triangle*> constructSceneTriangles(vector<vector<int>> vertexBuffer, vect
 }
 
 // assumes that we are positioning the camera further "back" on the y axis
-Point* computeCameraPosition(Point leftCorner, Point rightCorner){
+Point3* computeCameraPosition(Point3 leftCorner, Point3 rightCorner){
     double D = max(abs(rightCorner.x-leftCorner.x), abs(rightCorner.y-leftCorner.y), abs(rightCorner.z-leftCorner.z));
     
     // cameraPos = center + (0, -2D, 0)
     double x = (leftCorner.x + rightCorner.x)/2; 
-    double y = (leftCorner.y + rightCorner.y)/2 - 2*D; 
-    double z = (leftCorner.z + rightCorner.z)/2;
+    double y = (leftCorner.y + rightCorner.y)/2;
+    double z = (leftCorner.z + rightCorner.z)/2 - 2*D; 
+    
 
-    return new Point(x, y, z);
+    return new Point3(x, y, z);
     
 }
 
-HitInfo mollerTrumbore(Vector* ray, Triangle* tri) {
+HitInfo mollerTrumbore(Vector3* ray, Triangle* tri) {
     const double EPSILON = 1e-8;
 
     // Triangle edge vectors
@@ -128,7 +129,7 @@ HitInfo mollerTrumbore(Vector* ray, Triangle* tri) {
 
     // no hit (ray is close to parallel to triangle)
     if (fabs(det) < EPSILON) {
-        return HitInfo(false, Point(0, 0, 0), -1);
+        return HitInfo(false, Point3(0, 0, 0), -1);
     }
 
     double invDet = 1.0 / det;
@@ -141,7 +142,7 @@ HitInfo mollerTrumbore(Vector* ray, Triangle* tri) {
     // u barycentric coordinate
     double u = (tvecX * pvecX + tvecY * pvecY + tvecZ * pvecZ) * invDet;
     if (u < 0.0 || u > 1.0) { // no intersection
-        return HitInfo(false, Point(0, 0, 0), -1);
+        return HitInfo(false, Point3(0, 0, 0), -1);
     }
 
     // qvec = tvec x E1
@@ -152,17 +153,17 @@ HitInfo mollerTrumbore(Vector* ray, Triangle* tri) {
     // v barycentric coordinate
     double baryV = (ray->dx * qvecX + ray->dy * qvecY + ray->dz * qvecZ) * invDet;
     if (baryV < 0.0 || u + baryV > 1.0) {  // no intersection
-        return HitInfo(false, Point(0, 0, 0), -1);
+        return HitInfo(false, Point3(0, 0, 0), -1);
     }
 
     // ray parameter t
     double rayT = (e2x * qvecX + e2y * qvecY + e2z * qvecZ) * invDet;
     if (rayT < EPSILON) { // no intersection
-        return HitInfo(false, Point(0, 0, 0), -1);
+        return HitInfo(false, Point3(0, 0, 0), -1);
     }
 
     // Compute intersection point
-    Point intersection(
+    Point3 intersection(
         ray->p.x + rayT * ray->dx,
         ray->p.y + rayT * ray->dy,
         ray->p.z + rayT * ray->dz
@@ -171,10 +172,10 @@ HitInfo mollerTrumbore(Vector* ray, Triangle* tri) {
     return HitInfo(true, intersection, rayT);
 }
 
-Intersection findIntersectingTriangle(Vector ray, vector<Triangle*> sceneTriangles){
+Intersection findIntersectingTriangle(Vector3 ray, vector<Triangle*> sceneTriangles){
     double closestDistance = INFINITY;
     Triangle* tri = nullptr;
-    Point intersection(0, 0, 0);
+    Point3 intersection(0, 0, 0);
 
     for(int i = 0; i < sceneTriangles.size(); i++){
         HitInfo hit = mollerTrumbore(&ray, sceneTriangles[i]);
@@ -191,7 +192,7 @@ Intersection findIntersectingTriangle(Vector ray, vector<Triangle*> sceneTriangl
 
 }
 
-TriangleGrid findSurfaceIntersections(Point cameraPos, vector<Triangle*> sceneTriangles){
+TriangleGrid findSurfaceIntersections(Point3 cameraPos, vector<Triangle*> sceneTriangles){
 
     // define image plane metrics
     int imagePlaneDistance = 1;
@@ -209,12 +210,14 @@ TriangleGrid findSurfaceIntersections(Point cameraPos, vector<Triangle*> sceneTr
             let's call the point the pixel maps to m
             r/IMAGE_WIDTH = m_x/planeWidth; c/IMAGE_HEIGHT = m_z/planeHeight
             */
-            double m_x = (r+0.5)/IMAGE_WIDTH * planeWidth - planeWidth/2; // -planeWidth/2 since we want to center image screen at camera
-            double m_y = cameraPos.y + imagePlaneDistance;
-            double m_z = planeHeight/2 - (r + 0.5) / IMAGE_HEIGHT * planeHeight; // flip z since array goes top to bottom on z axis, world goes bottom to top
+            double m_x = (c + 0.5) / IMAGE_WIDTH  * planeWidth  - planeWidth / 2.0;
+            double m_y = planeHeight / 2.0 - (r + 0.5) / IMAGE_HEIGHT * planeHeight; // flip vertical because image coords go top->bottom but world goes bottom->top
+            double m_z = cameraPos.z + imagePlaneDistance; // image plane is in front of camera along +z
 
-            Vector v(cameraPos, cameraPos.x - m_x, m_y - cameraPos.y, cameraPos.z - m_z);
+            Point3 m(cameraPos.x + m_x, cameraPos.y + m_y, m_z); // world-space point on image plane
+            Vector3 v(cameraPos, m.x - cameraPos.x, m.y - cameraPos.y,m.z - cameraPos.z); // ray direction = m - camera
             triangleIntersections[r][c] = findIntersectingTriangle(v, sceneTriangles);
+            
         }
     }
 
